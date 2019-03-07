d7961aaeff7690d1dbbcd68ad3f8bf644e198e7f
Yubh8n <yubh8n.sdu@gmail.com>
2019-03-07 13:41:30 +0100

    We need RGB space!

--- PumpkinProject/venv/Test.py
+++ PumpkinProject/venv/Test.py
@@ -43,7 +43,7 @@ def backProj(referenceimg, original_image):
 # prints mean and standard deviation of either a color or B/W image
 def find_info(image, Colorimage):
     if Colorimage == True:
-        Colors = ["Blue", "Green", "Red"]
+        Colors = ["Channel 1", "Channel 2", "Channel 3"]
         for i in range (0,3):
             print Colors[i]
             print "Mean: " + str(np.mean(image[:,:,i]))
@@ -54,18 +54,12 @@ def find_info(image, Colorimage):
 
 def countPumpkins(binaryimage):
     contours, hierarchy = cv2.findContours(binaryimage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    A =  []
-    for i in range (0, np.size(contours)):
-        area = cv2.contourArea(contours[i])
-        if area > 0:
-            A.append(contours[i])
+    print "Number of contours found in the image. : " + str(np.size(contours))
 
-    print "\n Number of blobs in the image is: " + str(np.size(A))
-
-def MarkPumpkins(org_img, binaryimg):
+def MarkPumpkins(name, org_img, binaryimg):
     contours, hierarchy = cv2.findContours(binaryimg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(org_img, contours, -1, (0,0,255),3)
-    showimg("Marked pumpkins",org_img)
+    cv2.drawContours(org_img, contours, -1, (0,0,255),2)
+    showimg(name,org_img)
 
 def savePicture(imagename, image):
     cv2.imwrite(output+imagename+'.jpg', image)
@@ -88,7 +82,6 @@ def RGB_space3D(image):
     ax.set_zlim(0,255)
     plt.show()
 
-
 #Get reference pumpkins
 height= 12
 Pumpkin = picture[2193:2193+height, 2395:2395+height] # Reference image
@@ -96,65 +89,65 @@ pumpkins = Pumpkin
 Pumpkin = picture[1406:1406+height, 2598:2598+height] # Reference image
 pumpkins = np.hstack((Pumpkin,pumpkins))
 Pumpkin = picture[1125:1125+height, 2715:2715+height] # Reference image
-Ref_pumpkins = np.hstack((Pumpkin,pumpkins))
+Ref_pumpkins = np.hstack((Pumpkin,pumpkins)) #Stack those images beside each other.
 savePicture("Pumpkins", pumpkins)
 
 # exercise 1: find mean and standard deviations of the pumpkins.
 print "Find statistics of the RGB image; e.g mean and standard deviation"
-find_info(Ref_pumpkins, True)
+find_info(Ref_pumpkins, True)   #Print mean and standard deviations
 
-# segment the image from CieLab values.
-cieLab_ref = cv2.cvtColor(Ref_pumpkins, cv2.COLOR_BGR2LAB)
+# Find mean and standard deviation from CieLab values.
+cieLab_ref = cv2.cvtColor(Ref_pumpkins, cv2.COLOR_BGR2LAB)  #Convert to LAB image space
 print "\nFinding CieLAB image statistics"
-find_info(cieLab_ref, True)
+find_info(cieLab_ref, True)     #Print mean and standard deviations
 
-# Find pumpkins from thresholds.
-mask = cv2.inRange(picture, (30-(15*2),94-(22*2),170-(20*2)),(30+(15*2),94+(22*2),170+(20*2)))
-kernel = np.ones((9,9),np.uint8)
-mask1 = cv2.dilate(mask, kernel, iterations=1)
-#showimg("Finding blobs from RGB space", mask1)
-savePicture("RGB_Threshold", mask1)
-
-#Do a CieLAB color thresholding.
-cieLab_image = cv2.cvtColor(picture, cv2.COLOR_BGR2LAB)
-std_dev = 7
-cielab_segmented = cv2.inRange(cieLab_image, (122-(20*std_dev),154-(4*std_dev),175-(4*std_dev)),(122+(20*std_dev),154+(4*std_dev),175+(4*std_dev)))
-kernel = np.ones((9,9),np.uint8)
-cielab_segmented = cv2.morphologyEx(cielab_segmented, cv2.MORPH_OPEN, kernel)
-MarkPumpkins(picture, cielab_segmented)
-countPumpkins(cielab_segmented)
-
-#Stack images up uppon, so we can compare
-ret, bp_threshold = cv2.threshold(src=cielab_segmented,thresh=50,maxval=255,type=cv2.THRESH_BINARY)
-bp_threshold = cv2.merge((bp_threshold,bp_threshold,bp_threshold))
-res = cv2.bitwise_and(picture, bp_threshold)
-stack = np.vstack((picture, bp_threshold, res))
-savePicture("cieLab_8", stack)
-#Show the CieLAB color thresholding image.
-#showimg("CieLAB thresholding", stack)
 
 # exercise 2: Segment the image from different methods
+# Segment pumpkins from RGB thresholds.
+std_dev = 2
+mask = cv2.inRange(picture, (30-(15*std_dev),94-(22*std_dev),170-(20*std_dev)),(30+(15*std_dev),94+(22*std_dev),170+(20*std_dev)))
+# Show and save the result.
+showimg("Finding blobs from RGB space", mask)
+savePicture("RGB_Segmented", mask)
+
+#Do a CieLAB color segmented image by thresholding.
+cieLab_image = cv2.cvtColor(picture, cv2.COLOR_BGR2LAB) #Convert whole image to LAB image space
+std_dev = 8
+cielab_segmented = cv2.inRange(cieLab_image, (122-(20*15),154-(4*std_dev),175-(4*std_dev)),(122+(20*15),154+(4*std_dev),175+(4*std_dev)))
+savePicture("LAB_Segmented", cielab_segmented)
+showimg("LAB_Segmented", cielab_segmented)
+
 # use backprojection segmentation to find pumpkins.
 pumpkinsimage = backProj(Ref_pumpkins, picture)
-#showimg("Backprojected image", pumpkinsimage)
+showimg("Backprojected image", pumpkinsimage)
 savePicture("Backprojected_image", pumpkinsimage)
 
-Blue, Green, Red = cv2.split(Ref_pumpkins)
-Blue_mean = np.mean(Blue)
-Green_mean = np.mean(Green)
-Red_mean = np.mean(Red)
+#RGB Space distance thresholding.
 
-bgr = [Blue_mean,Green_mean,Red_mean]
-threshold = 50
-
-minBGR = np.array([bgr[0]-threshold, bgr[1]-threshold, bgr[2]-threshold])
-maxBGR = np.array([bgr[0]+threshold, bgr[1]+threshold, bgr[2]+threshold])
-binaryimg_RGB_distance = cv2.inRange(picture, minBGR, maxBGR)
-binaryimg_RGB_distance = cv2.merge((binaryimg_RGB_distance,binaryimg_RGB_distance,binaryimg_RGB_distance))
-result = cv2.bitwise_and(binaryimg_RGB_distance, picture)
-
-print math.sqrt(54**2+30**2)
+RGB_space3D(pumpkins)
 
 
-print np.shape(picture)
-#showimg("bgr distance", result)
+
+
+
+
+# Exercise 3
+
+# Find number of pumpkins in the LAB segmented image.
+print "cieLAB count pumpkins without filtering."
+countPumpkins(cielab_segmented)
+cielab_marked = picture.copy()
+MarkPumpkins("LAB image marked", cielab_marked, cielab_segmented)
+savePicture("LAB_Marked", cielab_marked)
+
+# Exercise 4 and 5 and 7
+
+# Filter the segmented image and save.
+print "\nFiltering the image and counting pumpkins \n"
+kernel = np.ones((5,5),np.uint8)
+cielab_segmented = cv2.morphologyEx(cielab_segmented, cv2.MORPH_OPEN, kernel)
+cielab_segmented_filtered = cv2.medianBlur(cielab_segmented, 7)
+LAB_filtered_marked = picture.copy()
+countPumpkins(cielab_segmented_filtered)
+MarkPumpkins("LAB filtered marked", LAB_filtered_marked,cielab_segmented_filtered)
+savePicture("LAB_filtered_marked", LAB_filtered_marked)
